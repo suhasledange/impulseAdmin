18066cdc8d0aa1cd94f3e7f3d5b687ea1a98d872
suhasledange <suhasledange10@gmail.com>
2024-10-30 15:14:22 +0530

    api bug fix

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -4,6 +4,7 @@ import { Controller } from "react-hook-form";
 import conf from "../util/conf";
 
 const RTE = ({ name, control, label, defaultValue = "" }) => {
+
   return (
     <div className="w-full">
       {label && <label className="inline-block mb-1 pl-1">{label}</label>}
--- src/util/conf.js
+++ src/util/conf.js
@@ -4,7 +4,7 @@ const conf = {
     APPWRITE_DATABASE_ID : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     APPWRITE_COLLECTION_ID : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     APPWRITE_BUCKET_ID : String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-    TINYMCE_KEY: String(import.meta.env.TINYMCE_KEY),
+    TINYMCE_KEY: String(import.meta.env.VITE_TINYMCE_KEY),
 }
 
 export default conf;
